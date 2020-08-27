a8508536855b0438547452d3c856d6ce8e7bb839
webtoor <fityanalimunshi@gmail.com>
2020-08-27 18:15:20 +0700

    create block.test

--- block.js
+++ block.js
@@ -1,17 +1,11 @@
 class Block {
-    constructor({timestamp, lastHast, hash, data}){
+    constructor({timestamp, lastHash, hash, data}){
         this.timestamp = timestamp;
-        this.lastHast = lastHast;
+        this.lastHash = lastHash;
         this.hash = hash;
         this.data = data
     }
 }
 
-const block1 = new Block({
-    data : "foo-data",
-    lastHast : "foo-lastHast",
-    hash : "foo-hash",
-    timestamp : "01/01/01"
-});
 
-console.log("block1", block1)
+module.exports = Block;
