ee48060b9d00476d5d4f4aaf8bfdc947fd9096d8
webtoor <fityanalimunshi@gmail.com>
2020-08-27 16:16:08 +0700

    create class Block + constructor

--- block.js
+++ block.js
@@ -1,5 +1,17 @@
 class Block {
-    constructor(){
-        
+    constructor({timestamp, lastHast, hash, data}){
+        this.timestamp = timestamp;
+        this.lastHast = lastHast;
+        this.hash = hash;
+        this.data = data
     }
 }
+
+const block1 = new Block({
+    data : "foo-data",
+    lastHast : "foo-lastHast",
+    hash : "foo-hash",
+    timestamp : "01/01/01"
+});
+
+console.log("block1", block1)
