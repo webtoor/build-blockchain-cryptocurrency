9e85615f5788de18203d4742888f944920fdcc47
webtoor <fityanalimunshi@gmail.com>
2020-12-04 16:47:27 +0700

    Sync Chain on Connect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub')
 
@@ -7,8 +8,11 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
+setTimeout(() => pubsub.broadcastChain(), 1000);
+ 
 app.use(bodyParser.json());
 
 app.get('/api/blocks', (req, res) => {
@@ -21,11 +25,21 @@ app.post('/api/mine', (req, res) => {
     blockchain.addBlock({ data });
 
     pubsub.broadcastChain();
-    
+
     res.redirect('/api/blocks');
 });
 
-const DEFAULT_PORT = 3000;
+const syncChain = () => {
+    request({ url : `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+
+            console.log('replace chain on a sync with', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+}
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true'){
@@ -33,4 +47,8 @@ if (process.env.GENERATE_PEER_PORT === 'true'){
 }
 
 const PORT = PEER_PORT || DEFAULT_PORT; 
-app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`listening at localhost:${PORT}`);
+
+    syncChain();
+});
