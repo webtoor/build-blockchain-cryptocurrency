015dd38f15d24ceea8cc945d86ffe20b432b0739
webtoor <fityanalimunshi@gmail.com>
2020-12-06 21:14:48 +0700

    Update Transactions with Multiple Outputs

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -74,7 +74,7 @@ describe('Transaction', () => {
             describe('and a transaction outputMap value is invalid', () => {
                 it('returns false and logs an error', () => {
                     transaction.outputMap[senderWallet.publicKey] = 999999;
-                    
+
                     expect(Transaction.validTransaction(transaction)).toBe(false);
                     expect(errorMock).toHaveBeenCalled();
                 });
@@ -89,6 +89,42 @@ describe('Transaction', () => {
 
                 });
             });
+        });
+    });
+
+    describe('update()', () => {
+        let originalSignature, originalSenderOutput, nextRecipient, nextAmount;
+
+        beforeEach(() => {
+            originalSignature = transaction.input.signature;
+            originalSenderOutput = transaction.outputMap[senderWallet.publicKey];
+            nextRecipient = 'next-recipient';
+            nextAmount = 50;
+
+            transaction.update({ 
+                senderWallet, recipient: nextRecipient, amount: nextAmount 
+            });
+
+        });
+
+        it('outputs the amount to the next recipient', () => {
+            expect(transaction.outputMap[nextRecipient]).toEqual(nextAmount);
+        });
+
+        it('subtracts the amount from the original sender output amount', () => {
+            expect(transaction.outputMap[senderWallet.publicKey]).toEqual(originalSenderOutput - nextAmount);
+        });
+
+        it('maintains a total output that matches the input amount', () => {
+            expect(
+            Object.values(transaction.outputMap)
+            .reduce((total, outputAmount) => total + outputAmount)
+            ).toEqual(transaction.input.amount);
+        });
+
+        it('re-signs the transaction', () => {
+            expect(transaction.input.signature).not.toEqual(originalSignature);
+
         });
     });
 });
