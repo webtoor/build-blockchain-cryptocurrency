3f5fbfa7f0f0bf6e721fe9402c040246bea60a18
webtoor <fityanalimunshi@gmail.com>
2020-12-08 20:46:35 +0700

    Get Transaction Pool Map

--- index.js
+++ index.js
@@ -53,6 +53,10 @@ app.post('/api/transact', (req, res) => {
     res.json({ type: 'success', transaction })
 });
 
+app.get('/api/transaction-pool-map', (req, res) => {
+    res.json(transactionPool.transactionMap);
+});
+
 const syncChain = () => {
     request({ url : `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
         if (!error && response.statusCode === 200) {
