c7d1b29fd26ed58029f4438e9431fb8c47388692
webtoor <fityanalimunshi@gmail.com>
2020-12-14 22:53:51 +0700

    Reward Transactions

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,6 +1,7 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
 const { verifySignature } = require('../util');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
 describe('Transaction', () => {
     let transaction, senderWallet, recipient, amount; 
@@ -154,6 +155,23 @@ describe('Transaction', () => {
                 expect(transaction.outputMap[senderWallet.publicKey])
                 .toEqual(originalSenderOutput - nextAmount - addedAmount);
             });
+        });
+    });
+
+    describe('rewardTransaction()', () => {
+        let rewardTransaction, minerWallet;
+
+        beforeEach(() => {
+            minerWallet = new Wallet();
+            rewardTransaction = Transaction.rewardTransaction({ minerWallet });
+        });
+
+        it('creates a transaction with the reward input', () => {
+            expect(rewardTransaction.input).toEqual(REWARD_INPUT);
+        });
+
+        it('creates ones transaction for the miner with `MINING_REWARD`', () => {
+            expect(rewardTransaction.outputMap[minerWallet.publicKey]).toEqual(MINING_REWARD);
         });
     });
 });
