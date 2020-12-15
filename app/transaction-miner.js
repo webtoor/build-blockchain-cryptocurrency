8f691d410f5a585526688bf885e413643fe11cff
webtoor <fityanalimunshi@gmail.com>
2020-12-15 20:48:03 +0700

    Mine Transactions Endpoint

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,3 +1,5 @@
+const Transaction = require('../wallet/transaction')
+
 class TransactionMiner{
     constructor({ blockchain, transactionPool, wallet, pubsub }){
         this.blockchain = blockchain;
@@ -8,14 +10,20 @@ class TransactionMiner{
     
     mineTransaction(){
         // get the transaction pool's valid transactions
-
+        const validTransaction = this.transactionPool.validTransaction();
+    
         // generate the miner's reward
-
+        validTransaction.push(
+            Transaction.rewardTransaction({ minerWallet: this.wallet })
+        );
         // add a block consisting of these transactions to the blockchain
+        this.blockchain.addBlock({ data: validTransaction });
 
         // broadcast the updated blockchain
+        this.pubsub.broadcastChain();
 
         // clear the pool
+        this.transactionPool.clear();
     }
 }
 
